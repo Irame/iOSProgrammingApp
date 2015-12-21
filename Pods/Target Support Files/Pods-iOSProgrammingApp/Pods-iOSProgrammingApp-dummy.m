d3994122423e8f3fdb22d6fdb6008b85499147d6
Irame <felix_keller@gmx.de>
2015-12-21 14:32:43 +0100

    added SwiftyJSON with cocoapods

---
+++ Pods/Target Support Files/Pods-iOSProgrammingApp/Pods-iOSProgrammingApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSProgrammingApp : NSObject
+@end
+@implementation PodsDummy_Pods_iOSProgrammingApp
+@end
---
+++ Pods/Target Support Files/Pods-iOSProgrammingApp/Pods-iOSProgrammingApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_iOSProgrammingAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iOSProgrammingAppVersionString[];
+
